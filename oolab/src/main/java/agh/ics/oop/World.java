9e963ce11392661dc7b6e5087c3f33fd538c56ff
od <oliwiadyszewska@gmail.com>
2024-10-28 11:41:14 +0100

    lab2

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,13 +1,14 @@
 package agh.ics.oop;
 
 import agh.ics.oop.model.MoveDirection;
+import agh.ics.oop.model.Vector2d;
 
 public class World {
     public static void main(String[] args) {
-        System.out.println("system wystartował");
+        System.out.println("Start");
         MoveDirection[] directions = OptionsParser.parse(args);
         run(directions);
-        System.out.println("system zakończył działanie");
+        System.out.println("Stop");
     }
     public static void run(MoveDirection[] directions){
         for (MoveDirection direction : directions) {
@@ -16,7 +17,6 @@ public class World {
                 case BACKWARD -> System.out.println("Zwierzak idzie do tyłu");
                 case RIGHT -> System.out.println("Zwierzak skręca w prawo");
                 case LEFT -> System.out.println("Zwierzak skręca w lewo");
-                default -> System.out.println("Ignorowany argument");
             }
         }
     }
---
+++ oolab/src/test/java/agh/ics/oop/model/Vector2dTest.java
@@ -0,0 +1,54 @@
+package agh.ics.oop.model;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class Vector2dTest {
+    @Test
+    void testEquals() {
+        assertEquals(new Vector2d(1, 2), new Vector2d(1, 2));
+        assertNotEquals(new Vector2d(1, 2), new Vector2d(2, 1));
+    }
+
+    @Test
+    void testToString() {
+        assertEquals("(1,2)", new Vector2d(1, 2).toString());
+    }
+
+    @Test
+    void testPrecedes() {
+        assertTrue(new Vector2d(1, 2).precedes(new Vector2d(2, 3)));
+        assertFalse(new Vector2d(3, 4).precedes(new Vector2d(2, 3)));
+    }
+
+    @Test
+    void testFollows(){
+        assertTrue(new Vector2d(3, 4).follows(new Vector2d(2, 3)));
+        assertFalse(new Vector2d(1, 2).follows(new Vector2d(2, 3)));
+    }
+
+    @Test
+    void testUpperRight() {
+        assertEquals(new Vector2d(3, 4), new Vector2d(1, 4).upperRight(new Vector2d(3, 2)));
+    }
+
+    @Test
+    void testLowerLeft() {
+        assertEquals(new Vector2d(1, 2), new Vector2d(1, 4).lowerLeft(new Vector2d(3, 2)));
+    }
+
+    @Test
+    void testAdd() {
+        assertEquals(new Vector2d(3, 5), new Vector2d(1, 2).add(new Vector2d(2, 3)));
+    }
+
+    @Test
+    void testSubtract() {
+        assertEquals(new Vector2d(-1, -1), new Vector2d(1, 2).subtract(new Vector2d(2, 3)));
+    }
+
+    @Test
+    void testOpposite() {
+        assertEquals(new Vector2d(-1, -2), new Vector2d(1, 2).opposite());
+    }
+}
